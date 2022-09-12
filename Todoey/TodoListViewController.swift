98d9ea42a50373b4c27c553b5e112f6329e8ae3c
Lucas Matheus <lmatheus@ciandt.com>
2022-09-12 13:34:31 -0300

    numberOfRows of tableViewController

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -1,13 +1,20 @@
 
 import UIKit
 
-class TodoListViewController: UIViewController {
-
+class TodoListViewController: UITableViewController {
+    
+    let itemArray = ["Apple", "Yellow", "Destroy man"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
        
     }
-
+    
+    
+    //MARK - TableView Datasource Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
 
 }
 
