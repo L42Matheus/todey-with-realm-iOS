18c7a40cc3aa2da87043645c290b43cad87da6d2
Lucas Matheus <lmatheus@ciandt.com>
2022-09-12 13:57:11 -0300

    Add accessoryType of checkmark in itemArray

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -25,7 +25,16 @@ class TodoListViewController: UITableViewController {
     
     //Mark - TableView Delegate Methods
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print(itemArray[indexPath.row])
+//        print(itemArray[indexPath.row])
+//
+        if  tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+
     }
     
     
